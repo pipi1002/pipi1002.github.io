ffe1166bb386cd691a1ff71fe40f301b07c69dcf
pipi1002 <wrr1111112022@163.com>
2022-12-31 11:33:51 +0800

    Site updated: 2022-12-31 11:33:50

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2022/11/16/C-C/","2022/11/16/C-Ctext/","2022/11/16/CSS样式/","2022/11/20/EXAM-NOTE/","2022/11/20/hello-world/","2022/11/16/页面homework/","2022/11/16/HTML/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2022/11/16/C-C/","2022/11/16/CSS样式/","2022/11/16/C-Ctext/","2022/11/20/EXAM-NOTE/","2022/11/20/hello-world/","2022/11/16/HTML/","2022/11/16/页面homework/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
