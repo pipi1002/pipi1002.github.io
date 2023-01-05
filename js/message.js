fc8d35c276aeba80575087d1ce2cb4af1dad1d8b
pipi1002 <wrr1111112022@163.com>
2023-01-05 13:35:18 +0800

    Site updated: 2023-01-05 13:35:17

--- js/message.js
+++ js/message.js
@@ -1,7 +1,7 @@
 new Vue({
     data: function () {
         this.$notify({
-            title: "，抓到你了🐖，欢迎来到pipi的blog😜",
+            title: "抓到你了🐖，欢迎来到pipi的blog😜，一起来学习吧！✨🎉",
             message: "小伙子，扒源记住要遵循GPL协议！有想问的欢迎评论哦📃",
             position: 'top-left',
             offset: 50,
